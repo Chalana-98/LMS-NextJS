8544bec979236f59c89398423aeb0697f9b483fb
Chalana Shehara <73055643+Chalana-98@users.noreply.github.com>
2023-11-08 13:32:46 +0530

    dashboard design
    dashboard design

--- app/(dashboard)/_components/sidebar-routs.tsx
+++ app/(dashboard)/_components/sidebar-routs.tsx
@@ -1,9 +1,35 @@
 "use client";
 
+import { Layout } from "lucide-react";
+import { SidebarItem } from "./sidebar-item";
+
+const guestRoutes = [
+    {
+        icon: Layout,
+        label:"Dashboard",
+        href:"/"
+    },
+    {
+        icon: Layout,
+        label:"Brows",
+        href:"/search"
+    },
+  
+]
+
 export const SidebarRouts = () => {
+  const routes = guestRoutes;
+  
     return (
         <div>
-            Routes!
+            {routes.map(route => (
+                <SidebarItem
+                key={route.href}
+                icon={route.icon}
+                label={route.label}
+                href={route.href}
+                />
+            ))}
         </div>
     )
 }
