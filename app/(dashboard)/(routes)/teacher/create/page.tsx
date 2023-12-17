bf644434982098545e89fb5a3de1f37e4f23f272
Chalana Shehara <73055643+Chalana-98@users.noreply.github.com>
2023-12-17 23:28:48 +0530

    Course form added
    Course form added

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -1,11 +1,83 @@
-"use client"
+"use client";
+
+import * as z from "zod";
+import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/router";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormLabel,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  title: z.string().min(1, {
+    message: "Tirlee is requires",
+  }),
+});
 
 const CreatePage = () => {
-    return ( 
-        <div>
-            Create Page
-        </div>
-     );
-}
- 
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+    },
+  });
+
+  const { isSubmitting, isValid } = form.formState;
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
+  return (
+    <div className=" max-w-5xl mx-auto flex md:items-center md:justify-items-center h-full p-6">
+      <div>
+        <h1 className=" text-2xl">
+          Name your course
+            </h1>
+          <p className=" text-sm text-slate-600">
+            What would you like to name course? Don&apos;t worry, you can change
+            this later.
+          </p>
+          <Form {...form}>
+            <form 
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-8 mt-8"
+            >
+               <FormField
+               control={form.control}
+               name="title"
+               render={({ field}) => (
+                <FormItem>
+                    <FormLabel>
+Course label
+                    </FormLabel>
+                    <FormControl>
+                        <Input 
+                        disabled={isSubmitting}
+                        placeholder="e.g. Advanced web development"
+                        {...field}
+                        />
+                    </FormControl>
+                    <FormDescription>
+                        What will you teach in this course
+                    </FormDescription>
+                </FormItem>
+               )}/>
+            </form>
+          </Form>
+       </div>
+    </div>
+  );
+};
+
 export default CreatePage;
